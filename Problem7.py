5a2e1daa454f635276a668dd9dacc475c1e6eb7f
ZuhaNadeem4 <140776560+ZuhaNadeem4@users.noreply.github.com>
2023-07-30 20:28:15 +0500

    Delete Problem7.py

--- Problem7.py
+++
@@ -1,4 +0,0 @@
-from os import listdir
-from os.path import isfile , join
-files_list=[f for f in listdir('/Users/Zuha Butt//Desktop') if isfile(join('/Users/Zuha Butt/Desktop',f))]
-print(files_list)
