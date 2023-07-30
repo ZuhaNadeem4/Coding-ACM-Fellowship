52f9f6e727c13a1a44ab201bb05beded30461534
ZuhaNadeem4 <140776560+ZuhaNadeem4@users.noreply.github.com>
2023-07-30 20:26:59 +0500

    Delete Problem3.py

--- Problem3.py
+++
@@ -1,7 +0,0 @@
-import time
-start_time=time.time();
-n=int(input('Enter the natural number upto which you want to find the sum:'));
-sum=(n*(n+1))/2;
-print("The sum is ",sum);
-end_time=time.time();
-print(end_time-start_time);
